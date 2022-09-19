08e3c30c73889f178c16b72a486073925fa87aa7
Mark <53943861+mdemore2@users.noreply.github.com>
2022-09-18 22:57:52 -0400

    project submission ready

---
+++ project/README.md
@@ -0,0 +1,9 @@
+## Udacity - C2- Infrastructure as Code - Project Submission
+To deploy stack, simply run create.bat or create.sh script and wait for deployment
+Note: network stack must be deployed before server stack due to references
+
+## Live Verification Link:
+http://servs-WebAp-1WZJAL46RS4CS-1037137366.us-east-1.elb.amazonaws.com
+
+## Stack Diagram
+![image] (Udacity-Infra-as-Code.png)
---
+++ project/create.sh
@@ -0,0 +1,7 @@
+echo aws cloudformation create-stack --stack-name netstack --template-body file://network.yml  --parameters file://network-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+aws cloudformation create-stack --stack-name netstack --template-body file://network.yml  --parameters file://network-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+
+sleep 5m
+
+echo aws cloudformation create-stack --stack-name servstack --template-body file://servers.yml  --parameters file://server-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
+aws cloudformation create-stack --stack-name servstack --template-body file://servers.yml  --parameters file://server-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
